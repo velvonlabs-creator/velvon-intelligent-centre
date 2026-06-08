20a4c914124dd09a5a16baa4e91c7196e3c0b3b9
velvonlabs-creator <velvonlabs@gmail.com>
2026-06-08 11:36:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollToCourses(){
+ document
+   .getElementById("courses")
+   .scrollIntoView({behavior:"smooth"});
+}
+
+function openWhatsapp(){
+ window.open(
+ "https://wa.me/919952045362"
+ );
+}
